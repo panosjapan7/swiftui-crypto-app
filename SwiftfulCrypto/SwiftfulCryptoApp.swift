fab3a29b2f70c74e208440d1340604f7fc00d770
panosjapan7 <panosjapan@gmail.com>
2023-09-03 08:50:35 +0200

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Panagiotis Tsapanidis on 2023-09-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
