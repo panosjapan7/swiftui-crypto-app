73923c9bfffc1c4bbdfd322a9313cbf07830c86b
panosjapan7 <panosjapan@gmail.com>
2023-09-04 09:07:55 +0200

    2. Creating HomeView and a navigation header

--- SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftfulCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeVIew()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
